7c3cc0df26ad63e5ffd943717a5ccc95486e91c2
aconfee <adamestela@gmail.com>
2019-04-16 18:28:07 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Form from './form.component';
-import Toolbar from '@material-ui/core/Toolbar';
+import './App.css';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const pinkStyle = {background:'#fce4ec', color:'#5C5C5C'};
+const styles = theme => ({
+  page: {
+    padding: '10px'
+  },
+  button: {
+    backgroundColor: '#FEDBD0',
+    color: '#442C2E',
+    width: '100%',
+    maxWidth: '650px',
+    minHeight: '60px',
+    borderRadius: '3px',
+    letterSpacing: '1px',
+    margin: '6px 0'
+  },
+  header: {
+    textAlign: 'center',
+    marginTop: '35px',
+    marginBottom: '35px',
+    color: '#442C2E',
+    fontSize: '1rem',
+    fontWeight: '500',
+    lineHeight: '1.25'
+  },
+  headerImage: {
+    maxWidth: '100px',
+    borderRadius: '50%',
+    marginBottom: '10px'
+  },
+  buttonContainer: {
+    textAlign: 'center'
+  },
+  lineBreak: {
+    borderTop: '1px solid #aeb0b1',
+    width: '75%',
+    maxWidth: '500px',
+    margin: '20px auto'
+  }
+});
 
 class App extends Component {
 
   render() {
-    return (
-      <div>
-        <AppBar position="static" style={pinkStyle}>
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              KimbyArting Giveaway!
-            </Typography>
-          </Toolbar>
-        </AppBar>
 
-        <Form onSubmit={this.handleSubmit} />
+    const { classes } = this.props;
+
+    return (
+      <div className={ classes.page }>
+        <div className={ classes.header }>
+          <div>
+            <img className={ classes.headerImage } src="./prof.jpg" />
+          </div>
+          <Typography 
+            variant="inherit"
+            color="inherit">
+            @kimbyarting
+          </Typography>
+        </div>
+
+        <div className={ classes.buttonContainer }>
+          <Button
+            className={ classes.button }
+            href="https://bit.ly/2S7cGEn"
+          >
+            STORE
+          </Button>
+          {/* <Button
+            className={ classes.button }
+            href="http://kimbyarting.com/"
+          >
+            COMMISSIONS
+          </Button> */}
+          <Button
+            className={ classes.button }
+            href="http://bit.ly/2ICe4xu"
+          >
+            YOUTUBE
+          </Button>
+          {/* <Button
+            className={ classes.button }
+            href="http://bit.ly/2IpBwyG"
+          >
+            PATREON
+          </Button> */}
+          <div className={ classes.lineBreak }></div>
+          <Button
+            className={ classes.button }
+            href="http://bit.ly/2Zdy3bL"
+          >
+            PORTFOLIO
+          </Button>
+          <Button
+            className={ classes.button }
+            href="http://kimgreenough.com/contact"
+          >
+            CONTACT
+          </Button>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
